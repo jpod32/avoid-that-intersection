c770f9a6afa6c3cfba12a1f62047a4aea53db808
jpod32 <jpodcat@gmail.com>
2023-03-10 21:15:07 -0800

    Add days, weeks, months

--- frontend/src/components/Overlay.tsx
+++ frontend/src/components/Overlay.tsx
@@ -1,15 +1,21 @@
 import { trpc } from "../utils/trpc"
 
-const relativeTime = (dateTime: number) => {
+export const relativeTime = (dateTime: number) => {
   const elapsed = new Date().getTime() - dateTime
 
   const seconds = Math.round(elapsed / 1000)
   const minutes = Math.round(elapsed / (1000 * 60))
   const hours = Math.round(elapsed / (1000 * 60 * 60))
+  const days = Math.round(elapsed / (1000 * 60 * 60 * 24))
+  const weeks = Math.round(elapsed / (1000 * 60 * 60 * 24 * 7))
+  const months = Math.round(elapsed / (1000 * 60 * 60 * 24 * 30))
 
   if (seconds < 60) return seconds + " seconds ago"
   if (minutes < 60) return minutes + " minutes ago"
   if (hours < 24) return hours + " hours ago"
+  if (days < 7) return days + " days ago"
+  if (weeks < 4) return weeks + " weeks ago"
+  return months + " months ago"
 }
 
 const Overlay = () => {
