1ecd4f4755594d09d44cd15d02a7ca93fd35262c
jpod32 <jpodcat@gmail.com>
2023-03-10 21:27:58 -0800

    Adjust gradient, custom intensities

--- frontend/src/components/HeatLayer.tsx
+++ frontend/src/components/HeatLayer.tsx
@@ -7,26 +7,38 @@ import type { AppRouter } from "../../../backend/src/router"
 
 type Incidents = inferProcedureOutput<AppRouter["getIncidents"]>
 
+const getIntensity = (incident: Incidents[number]["type"]) => {
+  switch (incident) {
+    case "Traffic Collision":
+      return 1
+    case "Expanded Traffic Collision":
+    case "Traffic Collision Involving Structure":
+    case "Traffic Collision Involving Train":
+      return 2
+    case "Multi Casualty":
+      return 3
+  }
+}
+
 const HeatLayer = ({ data }: { data: Incidents }) => {
   const map = useMap()
 
   const heatLayerData = data.map((incident) => [
     incident.lat,
     incident.lng,
-    1,
+    getIntensity(incident.type),
   ]) as HeatLatLngTuple[]
 
   L.heatLayer(heatLayerData, {
     gradient: {
-      0.4: "#910101",
-      0.6: "#c90000",
-      0.8: "#ff0000",
-      1: "#fc5050",
+      0.4: "#cc1821",
+      0.6: "#f13c37",
+      0.7: "#fc6b4b",
+      0.8: "#fc9272",
+      1.0: "#fcbba1",
     },
-    blur: 10,
-    max: 1,
-    minOpacity: 0.75,
-    radius: 10,
+    radius: 20,
+    minOpacity: 0.2,
   }).addTo(map)
 
   const dataCoordinates = data.map((incident) => [
